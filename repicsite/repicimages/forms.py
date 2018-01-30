5807fe642ca8f8cd232e007ee342d82a1bf5d318
stephen hipskind <stephen@stephen-macbook.hsd1.ca.comcast.net>
2018-01-29 20:01:59 -0800

    formatting

--- repicsite/repicimages/forms.py
+++ repicsite/repicimages/forms.py
@@ -5,7 +5,7 @@ from scrap.models import SubredditsList
 SUBREDDITS = [(item.id,item.subreddit) for item in SubredditsList.objects.all()]
 
 class SplashFilter(forms.Form):
-	choice_field = forms.MultipleChoiceField(
+    choice_field = forms.MultipleChoiceField(
         required=False,
         label='Choose what you want to see:',
         widget=forms.CheckboxSelectMultiple,
