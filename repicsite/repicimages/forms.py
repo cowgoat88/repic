9b075b077706c2ca5a8d2b0ebefc37563dda0088
cowgoat88 <kgoetz32@gmail.com>
2018-02-01 22:34:11 -0800

    nsfw filter

--- repicsite/repicimages/forms.py
+++ repicsite/repicimages/forms.py
@@ -1,13 +1,33 @@
 from django import forms
 from scrap.models import SubredditsList
 
-SUBREDDITS = [(item.id,item.subreddit) for item in SubredditsList.objects.all()]
+
+SUBREDDITS = [(item.id,item.subreddit) for item in SubredditsList.objects.filter(nsfw=0)]
+SUBREDDITS_NSFWONLY = [(item.id,item.subreddit) for item in SubredditsList.objects.filter(nsfw=1)]
+SUBREDDITS_ALL = [(item.id,item.subreddit) for item in SubredditsList.objects.all()]
+NSFW = [(True,'Include NSFW?')]
 
 class SplashFilter(forms.Form):
     choice_field = forms.MultipleChoiceField(
-        required=False,
-        label='',
-        widget=forms.CheckboxSelectMultiple(attrs={'class':'form-checkbox'}),
-        choices=SUBREDDITS,
+        required = False,
+        label = '',
+        widget = forms.CheckboxSelectMultiple(attrs={'class':'form-checkbox'}),
+        choices = SUBREDDITS,
     )
 
+class FilterAll(forms.Form):
+    choice_field = forms.MultipleChoiceField(
+        required=False,
+        label='',
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-checkbox'}),
+        choices=SUBREDDITS_ALL,
+    )
+
+
+class NsfwFilter(forms.Form):
+    nsfw_field = forms.MultipleChoiceField(
+        required = False,
+        label = '',
+        widget = forms.CheckboxSelectMultiple(attrs={'class': 'nfsw-checkbox'}),
+        choices = NSFW,
+    )
