065cddb87df4c9b8cd79a0161dcf8eb852f819d3
stephen hipskind <stephen@stephen-macbook.hsd1.ca.comcast.net>
2018-02-18 21:33:53 -0800

    SHIIIIITTT

---
+++ repicsite/repicimages/static/js/main.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+
+    function highlight(element) {
+      if (element.is('div')) {
+        if (element.hasClass('clicked')) {
+          element.removeClass('clicked');
+        } else {
+          element.addClass('clicked');
+        }
+        var subreddit = element.find('input')[0];
+        if (subreddit.checked == false) {
+          subreddit.checked = true;
+        } else {
+          subreddit.checked = false;
+        }
+      }
+    }
+
+    $('.subreddit').click(function(e) {
+        highlight($(this));
+    });
+
+    $(".subreddit li label input").click(function(e) {
+        e.stopPropagation();
+        highlight($(this).parent().parent());
+   });
+});
