64ce23ca03e777d21bdf2c3d2f2f0341e284cc39
cowgoat88 <kgoetz32@gmail.com>
2018-03-03 04:51:13 -0800

    working on clicker

--- repicsite/repicimages/views.py
+++ repicsite/repicimages/views.py
@@ -39,29 +39,32 @@ def images(request):
             return render(request, 'splash.html', context)
 
     elif request.method == "POST":
-
+        print(request.POST.getlist('nsfw_field'))
         if 'allow' in request.POST.getlist('nsfw_field'):
-            print(request.POST.getlist('nsfw_field'))
             context = {'nsfw_filter': False, 'filter': all_filter}
             return render(request, 'splash.html', context)
         elif 'only' in request.POST.getlist('nsfw_field'):
-            print(request.POST.getlist('nsfw_field'))
             context = {'nsfw_filter': False, 'filter': nsfw_only_filter}
             return render(request, 'splash.html', context)
         else:
-            subreddits = request.POST.getlist('choice_field')
-            links = Submission.objects.filter(subredditid__in=subreddits).order_by('-score')[:50]
-            subreddits = '!'.join(subreddits)
-
-            page = request.GET.get('page', 1)
-            paginator = Paginator(links, links_per_page)
             try:
-                numbers = paginator.page(page)
-            except PageNotAnInteger:
-                numbers = paginator.page(1)
-            except EmptyPage:
-                numbers = paginator.page(paginator.num_pages)
-            return render(request, 'images.html', {'links': numbers, 'subreddits': subreddits})
+                request.POST.getlist('choice_field')
+                subreddits = request.POST.getlist('choice_field')
+                links = Submission.objects.filter(subredditid__in=subreddits).order_by('-score')[:50]
+                subreddits = '!'.join(subreddits)
+
+                page = request.GET.get('page', 1)
+                paginator = Paginator(links, links_per_page)
+                try:
+                    numbers = paginator.page(page)
+                except PageNotAnInteger:
+                    numbers = paginator.page(1)
+                except EmptyPage:
+                    numbers = paginator.page(paginator.num_pages)
+                return render(request, 'images.html', {'links': numbers, 'subreddits': subreddits})
+            except AttributeError:
+                print('haha')
+
 
 
     return 'dummy face'
