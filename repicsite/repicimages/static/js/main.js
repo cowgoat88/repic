1d0dfe596b388a5314049007c33eaf68167179f2
cowgoat88 <kgoetz32@gmail.com>
2018-03-03 12:21:11 -0800

    fix on splash go button

--- repicsite/repicimages/static/js/main.js
+++ repicsite/repicimages/static/js/main.js
@@ -26,39 +26,4 @@ $(".subreddit li label input").click(function(e) {
     e.stopPropagation();
     highlight($(this).parent().parent());
    });
-
-
-$('.gifplayer');
-$('.gifplayer').gifplayer();
-
-$gifs = $('.gif');
-
-$gifs.each(function (i, gif) {
-    $(gif).data('isPlaying', false);
-});
-
-
-$(window).scroll(function () {
-    $gifs = $('.gif');
-
-    $gifs.each(function (i, gif) {
-        $gif = $(gif);
-
-        if ($gif.visible(true)) {
-            if (!$gif.data('isPlaying')) {
-                $gif.find('.gifplayer').gifplayer('play');
-                $gif.data('isPlaying', true);
-            }
-
-            if ($gif.find('.gp-gif-element').length > 1) {
-                $gif.find('.gp-gif-element').first().remove();
-            }
-        } else {
-            if ($gif.data('isPlaying')) {
-                $gif.find('.gifplayer').gifplayer('stop');
-                $gif.data('isPlaying', false);
-            }
-        }
-    });
-});
 });
