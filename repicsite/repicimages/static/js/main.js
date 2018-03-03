355c9408091d55083a83d70205854063f63971be
cowgoat88 <kgoetz32@gmail.com>
2018-03-03 11:48:43 -0800

    qwikfixs

--- repicsite/repicimages/static/js/main.js
+++ repicsite/repicimages/static/js/main.js
@@ -1,29 +1,14 @@
 
 $(document).ready(function() {
-      function highlight(element) {
-      if (element.is('li' || 'input')) {
-        console.log('li')
-        if (element.hasClass('clicked')) {
-          element.removeClass('clicked');
-        } else {
-          element.addClass('clicked');
-        }
-        var subreddit = element.find('input')[0];
-        if (subreddit.checked == false) {
-          subreddit.checked = true;
-        } else {
-          subreddit.checked = false;
-        }
-      }
-    }
+
     function highlight(element) {
-      if (element.is('div' || 'li' || 'input')) {
-        console.log('div')
+      if (element.is('div')) {
         if (element.hasClass('clicked')) {
           element.removeClass('clicked');
         } else {
           element.addClass('clicked');
         }
+        console.log(element.find('input'[0]))
         var subreddit = element.find('input')[0];
         if (subreddit.checked == false) {
           subreddit.checked = true;
@@ -33,15 +18,47 @@ $(document).ready(function() {
       }
     }
 
-
-
-    $('.subreddit').click(function(e) {
-        highlight($(this));
+$('.subreddit').click(function(e) {
+    highlight($(this));
     });
 
-    $(".subreddit li label input").click(function(e) {
-        e.stopPropagation();
-        highlight($(this).parent().parent());
-
+$(".subreddit li label input").click(function(e) {
+    e.stopPropagation();
+    highlight($(this).parent().parent());
    });
+
+
+$('.gifplayer');
+$('.gifplayer').gifplayer();
+
+$gifs = $('.gif');
+
+$gifs.each(function (i, gif) {
+    $(gif).data('isPlaying', false);
+});
+
+
+$(window).scroll(function () {
+    $gifs = $('.gif');
+
+    $gifs.each(function (i, gif) {
+        $gif = $(gif);
+
+        if ($gif.visible(true)) {
+            if (!$gif.data('isPlaying')) {
+                $gif.find('.gifplayer').gifplayer('play');
+                $gif.data('isPlaying', true);
+            }
+
+            if ($gif.find('.gp-gif-element').length > 1) {
+                $gif.find('.gp-gif-element').first().remove();
+            }
+        } else {
+            if ($gif.data('isPlaying')) {
+                $gif.find('.gifplayer').gifplayer('stop');
+                $gif.data('isPlaying', false);
+            }
+        }
+    });
+});
 });
