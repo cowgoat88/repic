0a125478e9f95bdf011fed545377259d639e7725
cowgoat88 <kgoetz32@gmail.com>
2018-03-03 17:42:26 -0800

    fix static

--- repicsite/repicsite/settings.py
+++ repicsite/repicsite/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'repicimages/static/css'),
 )
 # Local Static
-
+'''
 STATIC_URL = '/static/'
 
 
@@ -164,4 +164,3 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-'''
